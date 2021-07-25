bda01473963d6568765df79b3a0b0e087c172eef
Pedro Fidalgo <fidas.drokas@gmail.com>
2021-07-25 16:50:12 +0100

    Files reader

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,7 @@ namespace Console_App
     {
         public static void Tabuada()
         {
-            Console.WriteLine("===================================\n\n");
+            Console.WriteLine("===================================\n");
             Console.WriteLine("Digite um número intero: ");
 
             int num = int.Parse(Console.ReadLine());
@@ -20,9 +21,34 @@ namespace Console_App
             }
             Console.WriteLine("===================================");
         }
+
+        static private void LerArquivo(int numeroArquivo)
+        {
+            string arquivoComCaminho = @"C:\arquivos\arq" + numeroArquivo + ".txt";
+            if(File.Exists(arquivoComCaminho))
+            {
+                using (StreamReader arquivo = File.OpenText(arquivoComCaminho))
+                {
+                    Console.WriteLine("===================================\n");
+                    Console.WriteLine("File " + numeroArquivo + "\n");
+                    string linha;
+                    while ((linha = arquivo.ReadLine()) != null)
+                    {
+                        Console.WriteLine(linha);
+                    }
+                }
+            }
+            string arquivoComCaminho2 = @"C:\arquivos\arq" + (numeroArquivo + 1) + ".txt";
+            if (File.Exists(arquivoComCaminho2))
+            {
+                LerArquivo(numeroArquivo + 1);
+            }
+
+        }
         static void Main(string[] args)
         {
             Tabuada();
+            LerArquivo(1);
             Console.Read();
         }
     }
