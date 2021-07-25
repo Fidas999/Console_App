64f9cd568b8d77ddaa8ff1798dfdf8c0d14d3f94
Pedro Fidalgo <fidas.drokas@gmail.com>
2021-07-25 16:10:23 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_App
+{
+    class Program
+    {
+        public static void Tabuada()
+        {
+            Console.WriteLine("===================================\n\n");
+            Console.WriteLine("Digite um número intero: ");
+
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 1; i<=10; i++)
+            {
+                Console.WriteLine(num + " X " + i + " = " + (num*i));
+            }
+            Console.WriteLine("===================================");
+        }
+        static void Main(string[] args)
+        {
+            Tabuada();
+            Console.Read();
+        }
+    }
+}
